20616afb5b097d197c95f46643294943c3cf7127
Petr Huřťák <petr.hurtak@gmail.com>
2018-07-20 13:28:22 +0200

    new image + tests for images

--- src/images/images.js
+++ src/images/images.js
@@ -2,6 +2,7 @@ import image01 from "./images/01.jpg";
 import image02 from "./images/02.jpg";
 import image03 from "./images/03.jpg";
 import image04 from "./images/04.jpg";
+import image05 from "./images/05.jpg";
 
 export default [
   {
@@ -27,5 +28,11 @@ export default [
     name: null,
     location: null,
     source: "https://unsplash.com/photos/qKFxQ3X-YbI"
+  },
+  {
+    url: image05,
+    name: null,
+    location: "Hidden Lake Lookout trailhead, United States",
+    source: "https://unsplash.com/photos/RbbdzZBKRDY"
   }
 ];
