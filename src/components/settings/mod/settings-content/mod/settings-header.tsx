cef8a92accda1ae097c8558a71bf590a3ffce00a
Petr Huřťák <petr.hurtak@gmail.com>
2019-08-18 00:21:08 +0200

    rename from hello friend to hello

--- src/components/settings/mod/settings-content/mod/settings-header.tsx
+++ src/components/settings/mod/settings-content/mod/settings-header.tsx
@@ -5,13 +5,13 @@ import { Text, Heading } from "./styled";
 
 export const SettingsHeader = view(() => (
   <>
-    <Heading>Hello Friend</Heading>
+    <Heading>Hello</Heading>
 
     {!state.settings.cleanVersion && (
       <Text>
         How are you doing? Enjoy a nice background from Bing every day or take a look at some of the
-        fine backgrounds that I preselected. There is also a bunch of useful things that you can
-        display in front of the background, like clock and stuff!
+        backgrounds that I preselected. There is also a bunch of useful things that you can display
+        in front of the background, like clock and stuff!
       </Text>
     )}
   </>
