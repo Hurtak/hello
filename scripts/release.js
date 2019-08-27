9b5a9c634b0983b5d771c31c8dab4c0a5896dcf8
Petr Huřťák <petr.hurtak@gmail.com>
2019-08-27 14:44:45 +0200

    release script checkes changelog date, clenup of paths

--- scripts/release.js
+++ scripts/release.js
@@ -1,36 +1,32 @@
 const fs = require("fs");
 const path = require("path");
 const execa = require("execa");
+const dayjs = require("dayjs");
 
-async function execCommand(...args) {
-  let res;
-  try {
-    res = await execa(...args);
-  } catch (e) {
-    console.error(e.all);
-    process.exit(1);
-  }
-
-  return res.stdout;
-}
+const filePaths = {
+  rootDir: path.join(__dirname, ".."),
+  changelog: path.join(__dirname, "../CHANGELOG.md"),
+  packageJson: path.join(__dirname, "../package.json"),
+  manifest: path.join(__dirname, "../public/manifest.json"),
+};
 
 async function main() {
   console.log("Running release script");
 
-  process.chdir(path.join(__dirname, "/.."));
+  process.chdir(filePaths.rootDir);
   console.log(`CWD: ${process.cwd()}`);
 
   console.log("");
 
   console.log("Getting version from CHANGELOG.md");
-  const changelog = fs.readFileSync(path.join(__dirname, "../CHANGELOG.md"), "utf8");
-  const isUnreleased = Boolean(changelog.match(/^## \[Unreleased\]+/im));
+  const changelogString = fs.readFileSync(filePaths.changelog, "utf8");
+  const isUnreleased = Boolean(changelogString.match(/^## \[Unreleased\]+/im));
   if (isUnreleased) {
     console.error(`  "Unreleased" version in changelog, exiting`);
     return;
   }
 
-  const versionMatch = changelog.match(/^## \[(.+)\]+/m);
+  const versionMatch = changelogString.match(/^## \[(.+)\]+/m);
   const versionChangelog = Array.isArray(versionMatch) && versionMatch[1] ? versionMatch[1] : null;
   if (!versionChangelog) {
     console.error("  Could not parse version from changelog, exiting");
@@ -39,8 +35,44 @@ async function main() {
     console.log(`  OK, version: "${versionChangelog}"`);
   }
 
+  const gitTag = `v${versionChangelog}`;
+
+  console.log(`Check if tag "${gitTag}" does not already exist`);
+  const gitTagExists = await execCommand("git", ["tag", "--list", gitTag]);
+  if (gitTagExists) {
+    console.error(`  Git tag "${gitTag}" already exists, exiting`);
+    return;
+  } else {
+    console.log("  OK");
+  }
+
+  console.log(`Checking if there is date in CHANGELOG.md`);
+  const changelogDateMatch = changelogString.match(/^## \[.+\] - (\d{4}-\d{2}-\d{2})+/m);
+  const changelogDate =
+    Array.isArray(changelogDateMatch) && changelogDateMatch[1] ? changelogDateMatch[1] : null;
+  if (!changelogDate) {
+    console.error(`  Could not parse date from CHANGELOG.md, exiting`);
+    return;
+  } else {
+    console.log("  OK");
+  }
+
+  console.log(`Checking if there is current date in CHANGELOG.md`);
+
+  const expectedDateString = dayjs().format("YYYY-MM-DD");
+  if (expectedDateString !== changelogDate) {
+    console.error(
+      `  Date in CHANGELOG.md is not current date or it is in wrong format, expected ${expectedDateString}, given ${changelogDate}, exiting`,
+    );
+    return;
+  } else {
+    console.log("  OK");
+  }
+
+  process.exit(0);
+
   console.log(`Checking if there is correct version in package.json`);
-  const packageJsonString = fs.readFileSync(path.join(__dirname, "../package.json"), "utf8");
+  const packageJsonString = fs.readFileSync(filePaths.packageJson, "utf8");
   const packageJson = JSON.parse(packageJsonString);
   const versionPackageJson = packageJson.version;
   if (versionPackageJson !== versionChangelog) {
@@ -53,7 +85,7 @@ async function main() {
   }
 
   console.log(`Checking if there is correct version in public/manifest.json`);
-  const manifestString = fs.readFileSync(path.join(__dirname, "../public/manifest.json"), "utf8");
+  const manifestString = fs.readFileSync(filePaths.manifest, "utf8");
   const manifest = JSON.parse(manifestString);
   const versionManifest = manifest.version;
   if (versionManifest !== versionChangelog) {
@@ -82,17 +114,6 @@ async function main() {
   const gitStatus = await execCommand("git", ["status", "-s"]);
   if (gitStatus) {
     console.error(`  There are uncommitted changes, exiting`);
-    return;
-  } else {
-    console.log("  OK");
-  }
-
-  const gitTag = `v${versionChangelog}`;
-
-  console.log(`Check if tag "${gitTag}" does not already exist`);
-  const gitTagExists = await execCommand("git", ["tag", "--list", gitTag]);
-  if (gitTagExists) {
-    console.error(`  Git tag "${gitTag}" already exists, exiting`);
     return;
   } else {
     console.log("  OK");
@@ -128,6 +149,18 @@ async function main() {
 
   console.log("");
   console.log("DONE");
+}
+
+async function execCommand(...args) {
+  let res;
+  try {
+    res = await execa(...args);
+  } catch (e) {
+    console.error(e.all);
+    process.exit(1);
+  }
+
+  return res.stdout;
 }
 
 main();
